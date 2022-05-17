137e34e9a0450eaee60c3142f7e2ed887dbc0bcd
soohyuneeee <soohyuneeee12@gmail.com>
2022-05-17 12:30:38 +0900

    init

---
+++ src/main/java/com/example/firstuse/FirstuseApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstuseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstuse/FirstuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
