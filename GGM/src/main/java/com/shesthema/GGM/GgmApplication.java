9df601889446700496143edf18382abdb472e8a9
soohyuneeee <80656686+soohyuneeee@users.noreply.github.com>
2022-06-03 23:51:21 +0900

    Add files via upload

---
+++ GGM/src/main/java/com/shesthema/GGM/GgmApplication.java
@@ -0,0 +1,13 @@
+package com.shesthema.GGM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgmApplication.class, args);
+	}
+
+}
---
+++ GGM/src/test/java/com/shesthema/GGM/GgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shesthema.GGM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
